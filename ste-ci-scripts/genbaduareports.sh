d100b305686f564a15f273035c4bd8362d7a071a
StefanoBelli <stefano9913@gmail.com>
2024-05-26 21:35:51 +0200

    update CI, adding scripts

--- ste-ci-scripts/genbaduareports.sh
+++ ste-ci-scripts/genbaduareports.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-SUBPROJECT=bookkeeper-common-allocator
 BA_DUA_CLI=testdeps/ba-dua/ba-dua-cli-0.4.0-all.jar
 
-BA_DUA_CLASSES_PATH=$SUBPROJECT/target/classes/org/apache/bookkeeper/common/allocator/
-BA_DUA_SER=$SUBPROJECT/coverage.ser
+BA_DUA_CLASSES_PATH=$TESTED_SUBPROJECT/target/classes/$BA_DUA_CLASSES
+BA_DUA_SER=$TESTED_SUBPROJECT/coverage.ser
 BA_DUA_OUTXML=dataflow.xml
 
 echo " * Generating BaDua XML report"
