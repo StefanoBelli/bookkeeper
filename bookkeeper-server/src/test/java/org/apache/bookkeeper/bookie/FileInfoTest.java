1881bb5f58d0913a5f174ed3a22b9681e6bf92d6
StefanoBelli <stefano9913@gmail.com>
2024-06-03 23:08:24 +0200

    update tests

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/FileInfoTest.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/FileInfoTest.java
@@ -12,95 +12,115 @@ import java.nio.ByteBuffer;
 import static org.junit.Assert.*;
 
 public class FileInfoTest {
-    private final File logFile = new File("bklogs/test-bklog.bin");
-    private final byte[] masterKey = "bkIntegrityKey".getBytes();
-    private final int headerVersion = FileInfo.V1;
-
-    private FileInfo createFileInfo() throws IOException {
-        return new FileInfo(logFile, masterKey, headerVersion);
-    }
+    private static final File logFile = new File("bklogs/test-bklog.bin");
+    private static final byte[] masterKey = "bkIntegrityKey".getBytes();
+    private static final int headerVersion = FileInfo.V1;
 
     @After
-    public void deleteLogFile() {
+    public void postTest() {
         assertTrue(logFile.delete());
         assertTrue(logFile.getParentFile().delete());
     }
 
     @Test
-    public void testReadHeaderWhenLogFileDoesNotExist() throws IOException {
+    public void testCheckOpen() throws IOException {
         FileInfo fileInfo = createFileInfo();
+
         assertThrows(IOException.class, fileInfo::readHeader);
-
-        //avoid failure of @After-annotated method to delete log
-        //file after each test
-        logFile.mkdirs();
-        try {
-            logFile.createNewFile();
-        } catch(IOException ioe) {
-            if(!ioe.getMessage().equals("Is a directory")) {
-                throw ioe;
-            }
-        }
-    }
-
-    @Test
-    public void testExplicitLogFileCreationWhenDoesNotExist() throws IOException {
-        FileInfo fileInfo = createFileInfo();
 
         fileInfo.checkOpen(false);
         assertFalse(logFile.exists());
+        assertThrows(IOException.class, fileInfo::readHeader);
 
         fileInfo.checkOpen(true);
         assertTrue(logFile.exists());
+        checkEntireLogFileHeader(fileInfo);
 
         fileInfo.close(true);
     }
 
     @Test
-    public void testReadHeaderNoWrites() throws IOException {
+    public void testNullWrite() throws IOException {
         FileInfo fileInfo = createFileInfo();
-        assertThrows(IOException.class, fileInfo::readHeader);
+
+        assertThrows(NullPointerException.class, () -> fileInfo.write(null, 0));
+
         fileInfo.close(true);
     }
 
     @Test
-    public void testReadHeaderAfterWrite() throws IOException {
+    public void testFirstByteBufferInvalidWrite() throws IOException {
+        FileInfo fileInfo = createFileInfo();
+
+        byte[] data = "Hello world".getBytes();
+        ByteBuffer dataBuf = ByteBuffer.allocate(data.length);
+
+        assertThrows(NullPointerException.class,
+                () -> fileInfo.write(new ByteBuffer[] { null, dataBuf }, 0));
+
+        fileInfo.close(true);
+    }
+
+    @Test
+    public void testSecondByteBufferInvalidWrite() throws IOException {
+        FileInfo fileInfo = createFileInfo();
+
+        byte[] data = "Hello world".getBytes();
+        ByteBuffer dataBuf = ByteBuffer.allocate(data.length);
+
+        assertThrows(NullPointerException.class,
+                () -> fileInfo.write(new ByteBuffer[] { dataBuf, null }, 0));
+
+        fileInfo.close(true);
+    }
+
+    @Test
+    public void testCorrectSingleElementByteBufferWrite() throws IOException {
+        FileInfo fileInfo = createFileInfo();
+
+        byte[] data = "Hello world".getBytes();
+
+        ByteBuffer dataBuf = ByteBuffer.allocate(data.length);
+        dataBuf.put(data);
+        dataBuf.flip();
+        fileInfo.write(new ByteBuffer[] { dataBuf }, 0);
+
+        ByteBuffer dstBuf = ByteBuffer.allocate(data.length);
+        int effNReadBytes = fileInfo.read(dstBuf, 0, false);
+
+        assertEquals(data.length, effNReadBytes);
+
+        byte[] dataBack = new byte[data.length];
+        dstBuf.flip();
+        dstBuf.get(dataBack, 0, data.length);
+
+        assertEquals(new String(data), new String(dataBack));
+
+        fileInfo.close(true);
+    }
+
+    /*
+    @Test
+    public void testReadEntryAfterWrite() throws IOException {
         byte[] payloadData = "Hello world".getBytes();
         FileInfo fileInfo = createFileInfo();
 
         fileInfo.write(createLogEntry(0, 0, payloadData), 0);
 
-        fileInfo.readHeader();
+        ByteBuffer logEntry = ByteBuffer.allocate(payloadData.length);
+        fileInfo.read(logEntry, BK_ENTRY_META_LEN, false);
+
+        byte[] matchingPayload = new byte[payloadData.length];
+        logEntry.flip();
+        logEntry.get(matchingPayload, 0, payloadData.length);
+
         fileInfo.close(true);
 
-        assertTrue(true); //no exception was thrown
+        assertEquals(new String(payloadData), new String(matchingPayload));
     }
+     */
 
-    // 0: Signature [Integer]
-    // 4: HeaderVersion [Integer]
-    // 8: MasterKeyLengthInBytes [Integer]
-    // 12: MasterKey [VariadicSize]
-    // 12+MKLen: FencingStateBits [Integer]
-    private final int LOGFILE_BRIEFHEADER_V1_LENGTH =
-            Integer.BYTES * 4 + masterKey.length;
-
-    private static int bytesToInt(byte b0, byte b1, byte b2, byte b3) {
-        return b0 << 24 | b1 << 16 | b2 << 8 | b3;
-    }
-
-    private static byte[] extractBytes(int fromIdxIncl, int toExcl, byte[] largerBuf) {
-        byte[] buf = new byte[toExcl - fromIdxIncl];
-        for(int i = fromIdxIncl; i < toExcl; ++i) {
-            buf[i - fromIdxIncl] = largerBuf[i];
-        }
-
-        return buf;
-    }
-
-    @Test
-    public void testWholeLogFileHeader() throws IOException {
-        FileInfo fileInfo = createFileInfo();
-        fileInfo.checkOpen(true);
+    private static void checkEntireLogFileHeader(FileInfo fileInfo) throws IOException {
         try(FileInputStream fis = new FileInputStream(logFile)) {
             byte[] briefHeader = new byte[LOGFILE_BRIEFHEADER_V1_LENGTH];
             int nReadBytes = fis.read(briefHeader);
@@ -112,22 +132,15 @@ public class FileInfoTest {
             assertEquals('L', briefHeader[2]);
             assertEquals('E', briefHeader[3]);
 
-            int leHdrVer = bytesToInt(
-                    briefHeader[4],
-                    briefHeader[5],
-                    briefHeader[6],
-                    briefHeader[7]);
+            int leHdrVer = bytesToInt(briefHeader[4], briefHeader[5],
+                    briefHeader[6], briefHeader[7]);
             assertEquals(headerVersion, leHdrVer);
 
-            int leMkLen = bytesToInt(
-                    briefHeader[8],
-                    briefHeader[9],
-                    briefHeader[10],
-                    briefHeader[11]);
+            int leMkLen = bytesToInt(briefHeader[8], briefHeader[9],
+                    briefHeader[10], briefHeader[11]);
             assertEquals(masterKey.length, leMkLen);
 
-            assertEquals(
-                    new String(masterKey),
+            assertEquals(new String(masterKey),
                     new String(extractBytes(12, 12 + masterKey.length, briefHeader)));
 
             byte leState0 = briefHeader[12 + masterKey.length];
@@ -160,28 +173,36 @@ public class FileInfoTest {
         fileInfo.close(true);
     }
 
-    @Test
-    public void testReadEntryAfterWrite() throws IOException {
-        byte[] payloadData = "Hello world".getBytes();
-        FileInfo fileInfo = createFileInfo();
+    // test utils
 
-        fileInfo.write(createLogEntry(0, 0, payloadData), 0);
+    // 0: Signature [Integer]
+    // 4: HeaderVersion [Integer]
+    // 8: MasterKeyLengthInBytes [Integer]
+    // 12: MasterKey [VariadicSize]
+    // 12+MKLen: FencingStateBits [Integer]
+    private static final int LOGFILE_BRIEFHEADER_V1_LENGTH =
+            Integer.BYTES * 4 + masterKey.length;
 
-        ByteBuffer logEntry = ByteBuffer.allocate(payloadData.length);
-        fileInfo.read(logEntry, BK_ENTRY_META_LEN, false);
-
-        byte[] matchingPayload = new byte[payloadData.length];
-        logEntry.flip();
-        logEntry.get(matchingPayload, 0, payloadData.length);
-
-        fileInfo.close(true);
-
-        assertEquals(new String(payloadData), new String(matchingPayload));
+    private static int bytesToInt(byte b0, byte b1, byte b2, byte b3) {
+        return b0 << 24 | b1 << 16 | b2 << 8 | b3;
     }
 
+    private static byte[] extractBytes(int fromIdxIncl, int toExcl, byte[] largerBuf) {
+        byte[] buf = new byte[toExcl - fromIdxIncl];
+        for(int i = fromIdxIncl; i < toExcl; ++i) {
+            buf[i - fromIdxIncl] = largerBuf[i];
+        }
+
+        return buf;
+    }
+
+    private static FileInfo createFileInfo() throws IOException {
+        return new FileInfo(logFile, masterKey, headerVersion);
+    }
 
     private static final long BK_ENTRY_META_LEN = Long.BYTES * 2 + Integer.BYTES;
 
+    /*
     private static ByteBuffer[] createLogEntry(int ledgerId, int entryId, byte[] payload) {
         ByteBuffer bkMeta = ByteBuffer.allocate((int) BK_ENTRY_META_LEN);
         bkMeta.putLong(ledgerId);
@@ -195,4 +216,5 @@ public class FileInfoTest {
 
         return new ByteBuffer[] { bkMeta, bkPayload };
     }
+     */
 }
