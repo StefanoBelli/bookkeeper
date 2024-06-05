608433bfdb7e145c2ba42ef293609fa18bb6aa8f
StefanoBelli <stefano9913@gmail.com>
2024-06-05 20:19:21 +0200

    update test

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/net/NetworkTopologyTest.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/net/NetworkTopologyTest.java
@@ -12,56 +12,76 @@ import static org.junit.Assert.*;
 
 @RunWith(Parameterized.class)
 public class NetworkTopologyTest {
-
     @Parameterized.Parameters
     public static Iterable<Object[]> params() {
         return Arrays.asList(new Object[][] {
                 {null},
-                {"/zone-a/region-a/rack-1/node-1"},
-                {"/region-a/rack-1/node-1"},
-                {"/rack-1/node-1"},
-                {"/node-2"},
-                {"/"},
-                {""}
+                {new NodeWithItsArg(null)},
+                {new NodeWithItsArg("/zone-a/region-a/rack-1/node-1")},
+                {new NodeWithItsArg("/region-a/rack-1/node-1")},
+                {new NodeWithItsArg("/rack-1/node-1")},
+                {new NodeWithItsArg("/node-2")},
+                {new NodeWithItsArg("/")},
+                {new NodeWithItsArg("")}
         });
     }
 
-    private final String singlePath;
+    private final NodeWithItsArg nodeWithItsArg;
 
-    public NetworkTopologyTest(String singlePath) {
-        this.singlePath = singlePath;
+    public NetworkTopologyTest(NodeWithItsArg nodeWithItsArg) {
+        this.nodeWithItsArg = nodeWithItsArg;
     }
 
     @Test
     public void testSingleNodeAdd() {
         NetworkTopologyImpl networkTopology = new NetworkTopologyImpl();
-        final NodeBase node = singlePath != null ? new NodeBase(singlePath) : null;
+        final NodeBase singleNode = nodeWithItsArg != null ? nodeWithItsArg.createNodeBase() : null;
+        final String singlePath = nodeWithItsArg != null ? nodeWithItsArg.getPath() : null;
 
-        if(singlePath != null && singlePath.indexOf("/") == singlePath.lastIndexOf("/")) {
-            assertThrows(IllegalArgumentException.class, () -> networkTopology.add(node));
+        if(
+                singleNode != null && singlePath == null ||
+                (singlePath != null &&
+                        singlePath.indexOf("/") == singlePath.lastIndexOf("/"))) {
+
+            assertThrows(IllegalArgumentException.class, () -> networkTopology.add(singleNode));
         } else {
-            networkTopology.add(node);
-            int expNum = node != null ? 1 : 0;
+            networkTopology.add(singleNode);
+            int expNum = singleNode != null ? 1 : 0;
             assertEquals(expNum, networkTopology.getNumOfLeaves());
             assertEquals(expNum, networkTopology.getNumOfRacks());
             if(singlePath != null) {
                 int lastSlashIdx = singlePath.lastIndexOf("/");
-                assertEquals(node, networkTopology.getNode(singlePath));
+                assertEquals(singleNode, networkTopology.getNode(singlePath));
                 List<Node> nodesInRack = networkTopology.getDatanodesInRack(
                         singlePath.substring(0, lastSlashIdx));
                 if(nodesInRack.size() != 1) {
                     fail();
                 }
-                assertEquals(node, nodesInRack.get(0));
+                assertEquals(singleNode, nodesInRack.get(0));
 
                 int prevEnd = -1;
                 do {
                     prevEnd = singlePath.indexOf("/", prevEnd + 1);
                     String path = singlePath.substring(0, prevEnd == -1 ? lastSlashIdx : prevEnd);
                     Set<Node> nodes = networkTopology.getLeaves(path);
-                    assertTrue(nodes.size() == 1 && nodes.contains(node));
+                    assertTrue(nodes.size() == 1 && nodes.contains(singleNode));
                 } while(prevEnd != lastSlashIdx);
             }
+        }
+    }
+
+    public static final class NodeWithItsArg {
+        private final String path;
+        private NodeWithItsArg(String path) {
+            this.path = path;
+        }
+
+        public String getPath() {
+            return path;
+        }
+
+        public NodeBase createNodeBase() {
+            return new NodeBase(path);
         }
     }
 }
